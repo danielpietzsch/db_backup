b500d472660b7baaa35dfcb496ebbc0ad917872c
Daniel Pietzsch <daniel.pietzsch@trifectagis.com>
2011-04-29 17:13:29 +1200

    adding a db:restore recipe

--- recipes/db_backup.rb
+++ recipes/db_backup.rb
@@ -1,11 +1,19 @@
 namespace :db do
   
-  desc "Creates a backup of the database for the provided stage; use uncompressed format with compress=false."
+  desc "Creates a backup of the database for the provided stage; use compressed format with compress=true."
   task :backup do
-    compress = ENV['compress'] || 'true'
+    compress = ENV['compress'] || 'false'
     
-    # The rake task executed can be found at lib/tasks/backup.rake
+    # The rake task executed can be found at lib/tasks/db_backup.rake
     run("cd #{deploy_to}/current && rake RAILS_ENV=#{rails_env} db:backup compress=#{compress}")
+  end
+  
+  desc "Restores a backup of the database for the provided stage."
+  task :restore do
+    raise "You have to specify a file on the server to restore. E.g.: file=~/my_database_20110401162500.sql" if ENV['file'].nil?
+    
+    # The rake task executed can be found at lib/tasks/db_backup.rake
+    run("cd #{deploy_to}/current && rake RAILS_ENV=#{rails_env} db:restore file=#{ENV['file']}")
   end
   
 end
